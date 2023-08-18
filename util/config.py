a35c57f003099197b5bc888cd38000267534920c
Harsha Palli <harsha89vyas@gmail.com>
2023-08-17 20:33:50 -0400

    open

--- util/config.py
+++ util/config.py
@@ -1,2 +1,2 @@
 #create settings class to store constants like api key
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = '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'
